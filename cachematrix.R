f4959e10be1d80ec693f1df06f2cf9239938eac6
ECMira <87229759+ECMira@users.noreply.github.com>
2021-07-10 19:35:05 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  kirk <- NULL
+  set <- function(y){
+  maruts <<- y
+  kirk <<- NULL
+  }
+  get <- function()maruts
+  setInverse <- function(inverse) kirk <<- inverse
+  getInverse <- function() kirk 
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
 
+## this part of the code makes the inverse of special matrix computed.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(maruts, ...) {
+## Return a matrix that is the inverse of 'maruts'
+  kirk <- maruts$getInverse()
+  if(!is.null(kirk)){
+  message("getting cached data")
+  return(kirk)
+  }
+  mat <- maruts$get()
+  kirk <- solve(mat,...)
+  maruts$setInverse(kirk)
+  kirk
 }
